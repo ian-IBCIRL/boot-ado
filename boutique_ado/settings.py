e7b7d0e91dab37aa3128908b7bfa6791175e65d9
ian-IBCIRL <ian@ibcirl.com>
2023-04-08 20:58:58 +0000

    added confirm email to webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,6 +29,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
