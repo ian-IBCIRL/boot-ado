120aae9b7c76323d40f31b0478cf17af4130c4d0
ian-IBCIRL <ian@ibcirl.com>
2023-04-09 10:34:01 +0000

    Heroku Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ib-boutiqueado.wsgi:application
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,9 +42,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# modified allowed hosts to include the deployed heroku site
+ALLOWED_HOSTS = ['ib-boutiqueado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -140,13 +141,16 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
