6dfc70edf4947f5632aae0e7f3646e796a860dfd
ian-IBCIRL <ian@ibcirl.com>
2023-04-10 07:56:53 +0000

    added real email processing

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -75,7 +75,7 @@ INSTALLED_APPS = [
     
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -228,3 +228,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 FREE_DELIVERY_THRESHOLD = 100
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAILUSR')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAILPW')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAILUSR')
